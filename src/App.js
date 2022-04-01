da0c811f18b9285f218baceaa683724babe8511f
DESKTOP-JR2D966\Padres <dvdsanar@gmail.com>
2022-04-01 13:59:51 +0200

    Contenedor Registro de citas

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegistroUsuario from "./contenedores/RegisttroUsuario/RegistroUsuario.js";
 import ListadoUsuario from "./contenedores/ListadoUsuario/ListadoUsuario.js";
+import RegistroCita from "./contenedores/RegistroCitas/RegistroCitas.js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<RegistroUsuario />}></Route>
+          <Route path="/" element={<RegistroCita />}></Route>
           <Route path="/registro" element={<RegistroUsuario />}></Route>
           <Route path="/listado" element={<ListadoUsuario />}></Route>
+          <Route path="/registroCita" element={<RegistroCita />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
