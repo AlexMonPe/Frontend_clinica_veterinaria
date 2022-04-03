3c071080282bb979ba10945d9b36b0c68d6aabe4
Alejandro <Alex_bcn10@hotmail.es>
2022-04-03 15:07:40 +0200

    eliminado el mascotas.map de vista cliente y reemplazado por el componente InfoMascotas

--- src/contenedores/VistaCliente/VistaCliente.js
+++ src/contenedores/VistaCliente/VistaCliente.js
@@ -10,33 +10,7 @@ const VistaCliente = () => {
     return (
     <div>
       <div>{InfoUsuario}</div>
-      {mascotasCliente.map((mascota) => {
-        return (
-          <div>
-            <tr>
-              <th>Nombre de la mascota</th>
-              <th>peso</th>
-              <th>Fecha de nacimiento</th>
-              <th>Doctor</th>
-            </tr>
-            <tr>
-              <td>{mascota.nombre_mascota}</td>
-              <td>{mascota.peso}</td>
-              <td>{mascota.fecha_nacimiento}</td>
-              <td>{mascota.doctor}</td>
-            </tr>
-            <button type="button" onClick={CrearCita}>
-              Pedir cita
-            </button>
-            <button type="button" onClick={ModificarMascota}>
-              Modificar datos de mascota
-            </button>
-            <button type="button" onClick={BorrarMascota}>
-              Eliminar mascota :(
-            </button>
-          </div>
-        );
-      })}
+      <InfoMascotas />
     </div>
   );
 };
