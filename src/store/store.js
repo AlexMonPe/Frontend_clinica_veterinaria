e2890f1606ba2b8155147cbeae984027a3aaaeb3
David S <dvdsanar@gmail.com>
2022-04-06 17:41:42 +0200

    Logout redirigido y cambio de estado en login

--- src/store/store.js
+++ src/store/store.js
@@ -3,25 +3,31 @@ import { createStore } from "redux";
 
 const initialState = {
   logged: false,
-  popup: { visibilidad: false, texto: "" }
+  popup: { visibilidad: false, texto: "" },
 };
 
 const reductor = (state = initialState, action) => {
   console.log("llega A REDUCTOR");
-  if (action.type == "USER_LOGGED") {
+  if (action.type === "USER_LOGGED") {
     return {
       ...state,
       logged: true,
     };
   }
-  if (action.type == "VER_POPUP") {
+  if (action.type === "USER_LOGOUT") {
+    return {
+      ...state,
+      logged: false,
+    };
+  }
+  if (action.type === "VER_POPUP") {
     return {
       ...state,
       popup: { visibilidad: true, texto: action.payload },
     };
   }
 
-  if (action.type == "CERRAR_POPUP") {
+  if (action.type === "CERRAR_POPUP") {
     return {
       ...state,
       popup: { visibilidad: false },
